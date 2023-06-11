78a67458d93ad988108205bbbb28057758b29048
Buğra Kafa <135351301+BGR5@users.noreply.github.com>
2023-06-11 14:43:58 +0300

    Add files via upload

---
+++ JavaApplication6/src/javaapplication6/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication6;
+
+
+public class Calculator {
+    
+    
+    public double add(int x , int y){
+              return x + y;
+    }
+    
+    public double subtract(int x , int y ){
+            return x - y ; 
+     }    
+    
+    public double multiply(int x , int y ){
+           return x * y ;
+           
+    }
+    
+    public double divide (int x , int y ){
+         return x / y ;
+    }
+
+}
